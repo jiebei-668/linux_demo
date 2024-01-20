30f47e589b110dd8b624539432a5a76fdbb57951
jiebei <jiebeiya668@163.com>
2024-01-20 18:55:43 +0800

    客户端，服务端

---
+++ network/demo01.c
@@ -0,0 +1,84 @@
+/*****************************************************************************
+ * 程序名：demo01.c
+ * 作者：jiebei
+ * 功能：这是一个网络通信服务端，它接受一个客户端连接后将接受到的来自客户端的消息原封不动发送回去。Ctrl-c或kill可以终止该程序
+ ***************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#define MAX_BACKLOG  512
+
+int listen_sock = -1;
+int client_sock = -1;
+
+
+void cleanup(int sig)
+{
+	if(listen_sock != -1) close(listen_sock);
+	if(client_sock != -1) close(client_sock);
+	printf("server exit...\n");
+	exit(0);
+}
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		printf("Usage: ./demo01 port\n");
+		printf("example: ./demo01 8888\n");
+		return -1;
+	}
+	signal(2, cleanup);
+	signal(15, cleanup);
+	listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(listen_sock < 0)
+	{
+		printf("server socket() failed!\nerrno[%d]-[%s]\n", errno, strerror(errno));
+		return -1;
+	}
+	struct sockaddr_in skad;
+	memset(&skad, 0, sizeof(skad));
+	skad.sin_family = AF_INET;
+	skad.sin_addr.s_addr = inet_addr("127.0.0.1");
+	skad.sin_port = htons((in_port_t)atoi(argv[1]));
+
+
+	if(bind(listen_sock, (struct sockaddr *)&skad, sizeof(skad)) == -1)
+	{
+		printf("server bind() failed!\nerrno[%d]-[%s]\n", errno, strerror(errno));
+		close(listen_sock);
+		return -1;
+	}
+
+	if(listen(listen_sock, MAX_BACKLOG) == -1)
+	{
+		printf("server listen() failed!\nerrno[%d]-[%s]\n", errno, strerror(errno));
+		close(listen_sock);
+		return -1;
+	}
+	client_sock = accept(listen_sock, NULL, NULL);
+	if(client_sock == -1)
+	{
+		printf("server accept() failed!\nerrno[%d]-[%s]\n", errno, strerror(errno));
+		close(listen_sock);
+		return -1;
+	}
+	char buf[512];
+	while(1)
+	{
+		memset(buf, 0, sizeof(buf));
+		read(client_sock, buf, 512);
+		printf("recv: ==%s==\n", buf);
+		write(client_sock, buf, 512);
+		printf("write: ===%s===\n", buf);
+	}
+	close(listen_sock);
+	close(client_sock);
+	return 0;
+}
---
+++ network/demo02.c
@@ -0,0 +1,67 @@
+/*****************************************************************************
+ * 程序名：demo02.c
+ * 作者：jiebei
+ * 功能：这是一个网络通信客户端，它连接一个服务端后可以由终端向服务端发消息，并接收服务端的消息。Ctrl-c或kill可以终止该程序
+ ***************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int sockfd = -1;
+
+void cleanup(int sig)
+{
+	if(sockfd != -1) close(sockfd);
+	printf("client exit...\n");
+	exit(0);
+}
+int main(int argc, char* argv[])
+{
+	if(argc != 3)
+	{
+		printf("Usage: ./demo02 ip port\n");
+		printf("example: ./demo02 127.0.0.1 8888\n");
+		return -1;
+	}
+	signal(2, cleanup);
+	signal(15, cleanup);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+	{
+		printf("cilent socket() failed!\nerrno[%d]-[%s]\n", errno, strerror(errno));
+		return -1;
+	}
+	struct sockaddr_in skad;
+	memset(&skad, 0, sizeof(skad));
+	skad.sin_family = AF_INET;
+	skad.sin_addr.s_addr = inet_addr(argv[1]);
+	skad.sin_port = htons((in_port_t)atoi(argv[2]));
+
+
+	if(connect(sockfd, (struct sockaddr *)&skad, sizeof(skad)) == -1)
+	{
+		printf("cilent connect() failed!\nerrno[%d]-[%s]\n", errno, strerror(errno));
+		close(sockfd);
+		return -1;
+
+	}
+	char buf[512];
+
+	while(1)
+	{
+		memset(buf, 0, sizeof(buf));
+		scanf("%[^\n]%*c", buf);
+		write(sockfd, buf, 512);
+		read(sockfd, buf, 512);
+		printf("recv: ===%s===\n", buf);
+	}
+	close(sockfd);
+	return 0;
+}
